5f9d9c0e67b32d5c0b28b2a7c7153c71398c5fd5
Philip Top <top1@llnl.gov>
2019-10-18 11:11:53 -0700

    Update HELICS-FMI to use the HELICS shared Library and submodules

---
+++ config/cmake/addfmt.cmake
@@ -0,0 +1,31 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Copyright (c) 2017-2019, Battelle Memorial Institute; Lawrence Livermore
+# National Security, LLC; Alliance for Sustainable Energy, LLC.
+# See the top-level NOTICE for additional details.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# -----------------------------------------------------------------------------
+# create the fmt header only targets
+# -----------------------------------------------------------------------------
+
+set(FMT_SILENT ON)
+
+if(NOT CMAKE_CXX_STANDARD)
+    set(CMAKE_CXX_STANDARD 14) # Supported values are ``14``, and ``17``.
+endif()
+
+set(SUPPORTS_VARIADIC_TEMPLATES ON)
+set(SUPPORTS_USER_DEFINED_LITERALS ON)
+set(FMT_HAS_VARIANT OFF)
+
+# get the FMT header only library
+add_subdirectory(ThirdParty/fmtlib EXCLUDE_FROM_ALL)
+hide_variable(FMT_DOC)
+hide_variable(FMT_INSTALL)
+hide_variable(FMT_PEDANTIC)
+hide_variable(FMT_TEST)
+hide_variable(FMT_WERROR)
+hide_variable(FMT_FUZZ)
