007e22e6c25c366bd9848c3a7017c6d55ad04fdf
Philip Top <top1@llnl.gov>
2022-10-06 14:23:22 -0700

    add scripts and test out HELICS submodule build

--- config/cmake/addHELICSsubproject.cmake
+++ config/cmake/addHELICSsubproject.cmake
@@ -1,11 +1,11 @@
-#
-# Copyright (c) 2019, Battelle Memorial Institute; Lawrence Livermore
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Copyright (c) 2017-2022, Battelle Memorial Institute; Lawrence Livermore
 # National Security, LLC; Alliance for Sustainable Energy, LLC.
 # See the top-level NOTICE for additional details.
-#All rights reserved.
+# All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
-#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #
 # This file is used to add HELICS to a project
@@ -15,13 +15,13 @@
 
 string(TOLOWER "helics" lcName)
 
-if(NOT CMAKE_VERSION VERSION_LESS 3.11)
+
 include(FetchContent)
 
 FetchContent_Declare(
   helics
   GIT_REPOSITORY https://github.com/GMLC-TDC/HELICS.git
-  GIT_TAG       HELICS_EXPORT_TARGET
+  GIT_TAG       v3.3.0
 )
 
 FetchContent_GetProperties(helics)
@@ -31,31 +31,19 @@ if(NOT ${lcName}_POPULATED)
   FetchContent_Populate(helics)
 
 endif()
-else() #cmake <3.11
 
-# create the directory first
-file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/_deps)
-
-include(GitUtils)
-git_clone(
-             PROJECT_NAME                    ${lcName}
-             GIT_URL                         https://github.com/GMLC-TDC/HELICS.git
-             GIT_BRANCH                      HELICS_EXPORT_TARGET
-             DIRECTORY                       ${PROJECT_BINARY_DIR}/_deps
-       )
-
-set(${lcName}_BINARY_DIR ${PROJECT_BINARY_DIR}/_deps/${lcName}-build)
-
-
-
-endif()
 
   # Set custom variables, policies, etc.
   # ...
 
-  set(BUILD_HELICS_TESTS OFF CACHE BOOL "" FORCE)
-  set(BUILD_HELICS_EXAMPLES OFF CACHE BOOL "" FORCE)
-  set(BUILD_C_SHARED_LIB OFF CACHE BOOL "" FORCE)
+  set(HELICS_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+  set(HELICS_USE_EXTERNAL_FMT ON CACHE BOOL "" FORCE)
+  set(fmt_FOUND ON CACHE BOOL "" FORCE)
+  set(fmt_DIR ON CACHE BOOL "" FORCE)
+  set(HELICS_USE_EXTERNAL_JSONCPP ON CACHE BOOL "" FORCE)
+  set(HELICS_USE_EXTERNAL_UNITS ON CACHE BOOL "" FORCE)
+  set(HELICS_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+  set(HELICS_DISABLE_C_SHARED_LIB ON CACHE BOOL "" FORCE)
 add_subdirectory(${${lcName}_SOURCE_DIR} ${${lcName}_BINARY_DIR})
 
   #set_target_properties(clang-format clang-format-check clang-format-diff PROPERTIES FOLDER "Extern/zmq_clang_format")
