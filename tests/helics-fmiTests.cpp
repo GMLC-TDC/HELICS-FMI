006484d293695a08124776afe2dc5f6370b69fd2
Philip Top <phlptp@gmail.com>
2023-02-16 08:21:25 -0800

    add a few more tests

--- tests/helics-fmiTests.cpp
+++ tests/helics-fmiTests.cpp
@@ -64,7 +64,7 @@ TEST(loadtests, loadXML)
     EXPECT_FALSE(std::filesystem::exists(dir));
 }
 
-TEST(loadtests, loadSO)
+TEST(loadtests, loadSO_ME)
 {
     auto fmi = std::make_shared<FmiLibrary>();
     std::string inputFile = std::string(FMI_REFERENCE_DIR) + "BouncingBall.fmu";
@@ -81,4 +81,33 @@ TEST(loadtests, loadSO)
     EXPECT_EQ(fmiObj->getCurrentMode(), fmuMode::terminated);
     fmiObj.reset();
     fmi.reset();
+}
+
+
+TEST(loadtests, loadSO_CS)
+{
+    auto fmi = std::make_shared<FmiLibrary>();
+    std::string inputFile = std::string(FMI_REFERENCE_DIR) + "BouncingBall.fmu";
+    EXPECT_NO_THROW(fmi->loadFMU(inputFile));
+
+    auto fmiObj = fmi->createCoSimulationObject("model_cs");
+    ASSERT_TRUE(fmiObj);
+    EXPECT_EQ(fmiObj->getName(), "model_cs");
+
+    EXPECT_EQ(fmiObj->getCurrentMode(), fmuMode::instantiatedMode);
+    auto str = fmiObj->getInputNames();
+
+    fmiObj->setMode(fmuMode::terminated);
+    EXPECT_EQ(fmiObj->getCurrentMode(), fmuMode::terminated);
+    fmiObj.reset();
+
+    fmi->deleteFMUdirectory();
+
+    fmi.reset();
+
+    auto dir = std::string(FMI_REFERENCE_DIR) + "BouncingBall";
+    //this is true since we didn't open the directory in this test
+    EXPECT_TRUE(std::filesystem::exists(dir));
+
+    std::filesystem::remove_all(dir);
 }
