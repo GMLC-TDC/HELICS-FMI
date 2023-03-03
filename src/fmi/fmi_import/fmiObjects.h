db2d9b5d7a961befe46cba33986459d84490bde7
Philip Top <phlptp@gmail.com>
2023-03-03 10:48:52 -0800

    fix some clang-tidy issues

--- src/fmi/fmi_import/fmiObjects.h
+++ src/fmi/fmi_import/fmiObjects.h
@@ -167,7 +167,7 @@ class fmi2Object {
     void setInputVariables(const std::vector<int>& inIndices);
     bool addOutputVariable(const std::string& outputName);
     bool addOutputVariable(int index);
-    bool addInputVariable(const std::string& intputName);
+    bool addInputVariable(const std::string& inputName);
     bool addInputVariable(int index);
 
     fmiVariableSet getVariableSet(const std::string& variable) const;
