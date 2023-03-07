fc862a7e6a58ec851ecadb9e0f85d5c5e12a0126
Philip Top <phlptp@gmail.com>
2023-03-07 13:03:27 -0800

    start adding some tests of the exe wrapper

---
+++ tests/helicsFmiExecutableTests.cpp
@@ -0,0 +1,14 @@
+/*
+Copyright (c) 2017-2023,
+Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance
+for Sustainable Energy, LLC.  See the top-level NOTICE for additional details.
+All rights reserved. SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#include "exeTestHelper.h"
+#include "helics/application_api/queryFunctions.hpp"
+#include "helics/application_api/BrokerApp.hpp"
+
+#include "gtest/gtest.h"
+#include <filesystem>
+#include <future>
